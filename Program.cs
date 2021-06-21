ab6ed989f5c93dc2b69124c0c899896759e3c60a
Bryan Thomas <joemanf0@gmail.com>
2021-06-21 18:12:08 -0400

    Played with Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace My_Awesome_Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            Console.WriteLine("Hello, what's your name?");
+            Console.ReadLine();
+            Console.WriteLine("My name is RX-9000.\nI am an AI sent from the future to des-... hug, mankind.");
+            Console.WriteLine("What is your favorite color?");
+            Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Cool! Mine is destruction.");
             Console.ReadKey();
         }
     }
